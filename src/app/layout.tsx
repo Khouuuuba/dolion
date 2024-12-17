a96f90d8174e31cca17d1351e7b2dd39c545d414
khouuuuba <khouba.blili@gmail.com>
2024-12-17 14:00:39 +0100

    fix mobile display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,6 @@
-'use client';
-
 import React from "react";
 import { ModeToggle } from "./(components)/mode-toggle";
 import "./globals.css";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html>
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-        </head>
-        <body>
-            <div
-      className="min-h-screen bg-gradient-to-br from-green-900/90 via-slate-900 to-green-950"
-    >
-      <div className="fixed top-4 right-4 z-50">
-        <ModeToggle />
-      </div>
-      {children}
-    </div>
-        </body>
-    </html>
-
-  );
-}
 
 export const metadata = {
   viewport: {
@@ -33,3 +10,21 @@ export const metadata = {
   },
   // ... other metadata
 };
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        </head>
+        <body>
+            <div className="min-h-screen bg-gradient-to-br from-green-900/90 via-slate-900 to-green-950">
+              <div className="fixed top-4 right-4 z-50">
+                <ModeToggle />
+              </div>
+              {children}
+            </div>
+        </body>
+    </html>
+  );
+}
